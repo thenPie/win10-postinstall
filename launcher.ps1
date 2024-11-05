a5e7083a87f91818b6c5de6433d933ad40c5e8e6
thenPie <118397975+thenPie@users.noreply.github.com>
2024-11-05 17:31:31 +0300

    Update launcher.ps1

--- launcher.ps1
+++ launcher.ps1
@@ -3,8 +3,15 @@ $scriptUrl = "https://raw.githubusercontent.com/thenPie/win10-postinstall/main/R
 $isAdmin = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')
 
 if (-not $isAdmin) {
-    $command = "irm $scriptUrl | iex"
-    Start-Process powershell -ArgumentList "-NoProfile -ExecutionPolicy Bypass -Command $command" -Verb RunAs
+    try {
+        $command = "irm $scriptUrl | iex"
+        Start-Process powershell -ArgumentList "-NoProfile -ExecutionPolicy Bypass -Command $command" -Verb RunAs -ErrorAction Stop
+    }
+    catch {
+        Write-Host "`nElevation denied. Admin rights are required to run this script." -ForegroundColor Red
+        Write-Host "Press any key to exit..."
+        $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown')
+    }
 }
 else {
     . { iwr -useb $scriptUrl } | iex
