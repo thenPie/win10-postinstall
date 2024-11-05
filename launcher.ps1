495555e252733f63fa0b65cfbc348d5148fdfdf7
thenPie <118397975+thenPie@users.noreply.github.com>
2024-11-05 17:29:28 +0300

    Update launcher.ps1

--- launcher.ps1
+++ launcher.ps1
@@ -1,11 +1,11 @@
+$scriptUrl = "https://raw.githubusercontent.com/thenPie/win10-postinstall/main/Removal.ps1"
+
 $isAdmin = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')
 
-$scriptUrl = "https://raw.githubusercontent.com/thenPie/win10-postinstall/refs/heads/main/Removal.ps1"
-
-if ($isAdmin) {
-    irm $scriptUrl | iex
-}
-else {
+if (-not $isAdmin) {
     $command = "irm $scriptUrl | iex"
     Start-Process powershell -ArgumentList "-NoProfile -ExecutionPolicy Bypass -Command $command" -Verb RunAs
+}
+else {
+    . { iwr -useb $scriptUrl } | iex
 }
