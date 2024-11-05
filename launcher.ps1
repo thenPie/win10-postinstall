5d8827ca9048c128e1c1a43b1a98d638e371a3d3
thenPie <118397975+thenPie@users.noreply.github.com>
2024-11-05 17:43:36 +0300

    Update launcher.ps1

--- launcher.ps1
+++ launcher.ps1
@@ -3,8 +3,10 @@ $scriptUrl = "https://raw.githubusercontent.com/thenPie/win10-postinstall/main/R
 $isAdmin = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')
 
 if (-not $isAdmin) {
+    $proc = Start-Process powershell -ArgumentList "-NoProfile -ExecutionPolicy Bypass -Command `"iwr -useb $scriptUrl | iex`"" -Verb RunAs
     Write-Host "`nAdmin rights are required to run this script." -ForegroundColor Yellow
-    Start-Process powershell -ArgumentList "-NoProfile -ExecutionPolicy Bypass -Command `"iwr -useb $scriptUrl | iex`"" -Verb RunAs
+    Write-Host "Press any key to exit..."
+    $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown')
 }
 else {
     . { iwr -useb $scriptUrl } | iex
