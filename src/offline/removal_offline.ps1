d8486f4ceb6778eb7538a6da461e63e303a163ff
thenPie <118397975+thenPie@users.noreply.github.com>
2025-03-14 22:46:55 +0300

    provisioned appxs to be removed

--- src/offline/removal_offline.ps1
+++ src/offline/removal_offline.ps1
@@ -3,3 +3,9 @@ $appxs = Get-Content -Path '.\to_delete_appxs.txt'
 ForEach ($appx in $appxs) {
     Get-AppxPackage $appx | Remove-AppxPackage
 }
+
+$provisioned_appxs = Get-Content -Path '.\provisioned_appxs_to_stop.txt'
+
+ForEach ($provisioned_appx in $provisioned_appxs) {
+    Remove-AppxProvisionedPackage -Online -PackageName $provisioned_appx
+}
