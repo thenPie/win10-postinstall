0b8262ace68e72ca7bc82e98aeee0f0e36d08ae0
thenPie <118397975+thenPie@users.noreply.github.com>
2025-03-14 22:03:53 +0300

    try again script

--- src/Removal.ps1
+++ src/Removal.ps1
@@ -1,4 +1,31 @@
-$appxs = Get-Content -Path '.\to_delete_appxs.txt'
+# $appxs = Get-Content -Path '.\to_delete_appxs.txt'
+
+$appxs = [System.Collections.Generic.List[string]]::new()
+
+$appxs.Add("Microsoft.Services.Store.Engagement*")
+$appxs.Add("Microsoft.Advertising*")
+$appxs.Add("Microsoft.549981C3F5F10*")
+$appxs.Add("Microsoft.WindowsCamera*")
+$appxs.Add("Microsoft.WindowsAlarms*")
+$appxs.Add("Microsoft.SkypeApp*")
+$appxs.Add("Microsoft.ZuneVideo*")
+$appxs.Add("Microsoft.ZuneMusic*")
+$appxs.Add("Microsoft.YourPhone*")
+$appxs.Add("Microsoft.WindowsSoundRecorder*")
+$appxs.Add("Microsoft.WindowsMaps*")
+$appxs.Add("Microsoft.WindowsFeedbackHub*")
+$appxs.Add("Microsoft.Wallet*")
+$appxs.Add("Microsoft.People*")
+$appxs.Add("Microsoft.Office.OneNote*")
+$appxs.Add("Microsoft.MixedReality.Portal*")
+$appxs.Add("Microsoft.MicrosoftStickyNotes*")
+$appxs.Add("Microsoft.MicrosoftSolitaireCollection*")
+$appxs.Add("Microsoft.MicrosoftOfficeHub*")
+$appxs.Add("Microsoft.Microsoft3DViewer*")
+$appxs.Add("Microsoft.Getstarted*")
+$appxs.Add("Microsoft.GetHelp*")
+$appxs.Add("microsoft.windowscommunicationsapps*")
+$appxs.Add("Microsoft.BingWeather*")
 
 ForEach ($appx in $appxs) {
     Get-AppxPackage $appx | Remove-AppxPackage
